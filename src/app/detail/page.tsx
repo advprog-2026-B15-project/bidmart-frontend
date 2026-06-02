1564909eb1dcb22966caa6594232d28868bef37f
unknown <seanmarcello836@gmail.com>
2026-06-03 02:31:51 +0700

    revert lagi

--- src/app/detail/page.tsx
+++ src/app/detail/page.tsx
@@ -67,6 +67,7 @@ function DetailContent() {
   const safe = cd.total > 60 * 60 * 1000;
   const urgent = cd.total < 2 * 60 * 1000 && cd.total > 0;
 
+
   const mainImageUrl = auctionItem?.imageUrls?.[thumb]
     ? `/api/proxy/uploads/${auctionItem.imageUrls[thumb].split('/').pop()}`
     : null;
